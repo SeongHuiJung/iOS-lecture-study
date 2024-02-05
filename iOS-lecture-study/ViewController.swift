f5eaef706abbfa915aa743505b4c935721769ae1
Jseong <tjdgml150237@naver.com>
2024-02-05 15:00:56 +0900

    Initial Commit

---
+++ iOS-lecture-study/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS-lecture-study
+//
+//  Created by 정성희 on 2024/02/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
